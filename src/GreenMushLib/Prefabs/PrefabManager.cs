7028b56c38f5e06fde63261488b4e806a67eaaed
Robyn <rnixon98@gmail.com>
2024-12-03 12:43:19 -0600

    docs: Adds XML docs

--- src/GreenMushLib/Prefabs/PrefabManager.cs
+++ src/GreenMushLib/Prefabs/PrefabManager.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace GreenMushLib.Prefabs;
 
+/// <summary>
+/// A static class that contains lists of game prefabs sorted by name
+/// </summary>
 public static class PrefabsManager
 {
     private static readonly Dictionary<string, GameObject> CollectablePrefabList = [];
@@ -18,6 +21,16 @@ public static class PrefabsManager
         }
     }
 
+    /// <summary>
+    /// Retrieves a collectable object from the list by name
+    /// </summary>
+    /// <param name="name">This is the game object's name</param>
+    /// <example>
+    /// <code>
+    /// GetCollectablePrefabByName("Can opener");
+    /// </code>
+    /// </example>
+    /// <returns>A GameObject or null if none is found</returns>
     public static GameObject? GetCollectablePrefabByName(string name)
     {
         if (CollectablePrefabList.TryGetValue(name, out var thing))
