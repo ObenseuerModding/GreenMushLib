61bf602cce4a085e8e0cdb63165243bbdaae055a
Robyn <rnixon98@gmail.com>
2024-12-05 16:15:52 -0600

    feat: example command 'echo'

---
+++ src/GreenMushLib/Commands/EchoCommand.cs
@@ -0,0 +1,21 @@
+namespace GreenMushLib.Commands;
+
+internal class TestCommand : IConsoleCommand
+{
+    public string Name => "echo";
+
+    public string Description => "echoes whatever you tell it to";
+
+    public string Usage => "echo <string>";
+
+    public void Execute(params string[] args)
+    {
+        if (args.Length < 2)
+        {
+            CommandManager.PrintToDevConsole(Usage);
+            return;
+        }
+
+        CommandManager.PrintToDevConsole(args[1].Trim('"'));
+    }
+}
